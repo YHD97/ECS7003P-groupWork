e90333464f4dee2eeb64f10446672441699f5657
LIU CHENG <ec21108@qmul.ac.uk>
2021-10-30 11:23:15 +0100

    Fix
    Fixed the bug of player turning when it hit the corner of other object.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,11 +8,9 @@ public class PlayerController : MonoBehaviour
     public float movespeed = 3;
     public float jumpspeed = 10;
     public float timeVal;
-    private bool faceLeft = true;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Ridgidbody2D>();  
     }
 
     // Update is called once per frame
@@ -36,17 +34,5 @@ public class PlayerController : MonoBehaviour
         float v = Input.GetAxisRaw("Jump");
         transform.Translate(Vector3.up * v * jumpspeed * Time.deltaTime, Space.World);
 
-        if(faceLeft)
-        {
-            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
-            if(Vector3.right<0)
-            {
-                faceLeft = false;
-            }
-            else
-            {
-                faceLeft = true;
-            }
-        }
     }
 }
