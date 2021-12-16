e2657451d574aa48ce3b8721151b243710d571a4
JSSan97 <j.san@hotmail.co.uk>
2021-12-16 16:57:21 +0000

    Add particles on death

--- Assets/Script/DeathScript.cs
+++ Assets/Script/DeathScript.cs
@@ -1,3 +1,5 @@
+/* Attach this script the player */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +9,18 @@ using UnityEngine.SceneManagement;
 public class DeathScript : MonoBehaviour
 {
     public Text gameoverText;
+    public GameObject deathParticles;
+
+    private GameObject instantiatedParticles;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "enemy" || collision.gameObject.tag == "Trap" || collision.gameObject.tag == "Fireball" || collision.gameObject.tag == "Acid Drop"){
             if(collision.gameObject.tag == "Fireball" || collision.gameObject.tag == "Acid Drop"){
                 Destroy(collision.gameObject);
             }
+            instantiatedParticles = Instantiate(deathParticles, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(instantiatedParticles, 5.0f);
             gameoverText.text = "GAME OVER!";
             this.gameObject.SetActive(false);
             Invoke("restartLevel", 2);
