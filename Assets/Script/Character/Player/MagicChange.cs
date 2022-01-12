0a3d3f48b264e89eeb3cf92ea813ec41a0a71d24
LIU CHENG <ec21108@qmul.ac.uk>
2022-01-12 19:33:27 +0000

    Fix some bugs
    1. Fix bug for magic hat could not disappear normally.
    
    2. Fix bug for magic change could not run normally.

--- Assets/Script/Character/Player/MagicChange.cs
+++ Assets/Script/Character/Player/MagicChange.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagicChange : MonoBehaviour
+public class magicchange : MonoBehaviour
 {
 
     private Animator anim;
--- Assets/Script/Character/Player/magichat.cs
+++ Assets/Script/Character/Player/magichat.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class magicgat : MonoBehaviour
-
-
+public class magichat : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         
     }
@@ -16,7 +20,7 @@ public class magicgat : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(gameObject,0.1f);
+            Destroy(gameObject, 0.1f);
         }
     }
 }
